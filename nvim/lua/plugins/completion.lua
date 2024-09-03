23ebd41bc22cc6a14bf8f9c482a6f87dd12ab024
Jms Dnns <jdennis@gmail.com>
2024-09-03 12:06:21 -0400

    Adding an excellent autocomplete setup

---
+++ nvim/lua/plugins/completion.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.preselect = cmp.PreselectMode.None
+      opts.completion = {
+        completeopt = "noselect",
+      }
+      opts.mapping = cmp.mapping.preset.insert({
+        ["<Down>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+        ["<Up>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+        ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+        ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+        ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+        ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+        ["<C-e>"] = cmp.mapping({
+          i = cmp.mapping.abort(),
+          c = cmp.mapping.close(),
+        }),
+        -- Enter only completes for explicitly selected items
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+      })
+    end,
+  },
+}
