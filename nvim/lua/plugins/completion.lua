b718b72a6d8b7e597d95b6a135af0942774aa1f6
Jms Dnns <jdennis@gmail.com>
2024-09-21 20:42:55 -0400

    Finally got neovim to stop showing me snippets. Yowza they're so annoying.

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -2,10 +2,14 @@ return {
   {
     "hrsh7th/nvim-cmp",
     opts = function(_, opts)
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      capabilities.textDocument.completion.completionItem.snippetSupport = false
+
       local cmp = require("cmp")
       opts.preselect = cmp.PreselectMode.None
       opts.completion = {
         completeopt = "noselect",
+        completionItem = { snippetSupport = false },
       }
       opts.mapping = cmp.mapping.preset.insert({
         ["<Down>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
@@ -21,11 +25,13 @@ return {
         -- Enter only completes for explicitly selected items
         ["<CR>"] = cmp.mapping.confirm({ select = false }),
       })
+      opts.snippet = {
+        expand = function(item)
+          return ""
+        end,
+      }
     end,
   },
-  {
-    "garymjr/nvim-snippets",
-    -- Blows my mind anyone likes this
-    enabled = false,
-  },
+  { "garymjr/nvim-snippets", enabled = false },
+  { "rafamadriz/friendly-snippets", enabled = false },
 }
