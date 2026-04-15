7d4a6f83f591fd0c46c0e1c47230faf963ec31ae
Jms Dnns <jdennis@gmail.com>
2026-04-15 16:05:13 -0400

    Updated neovim to latest config format

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,12 +1,20 @@
 -- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+-- Default options that are always set
 
+-- Disorienting
 vim.opt.relativenumber = false
+
+-- Half tab
 vim.opt.tabstop = 4
+
+-- Tabs as spaces
 vim.opt.expandtab = true
+
+-- Square cursors
 vim.opt.guicursor = "n-v-i-c:block-Cursor"
 
 -- Python
-vim.g.lazyvim_python_lsp = "pyright"
-vim.g.lazyvim_python_ruff = "ruff"
+-- vim.g.lazyvim_python_lsp = "pyright"
+-- vim.g.lazyvim_python_ruff = "ruff"
+
+vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
